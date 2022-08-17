9396c35e1a6d399ba246cc840cab2022ff4c59d9
Roman <igoshinr.estho@gmail.com>
2022-08-17 03:31:30 +0300

    47

---
+++ 47/Program.cs
@@ -0,0 +1,15 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[m, n];
+double x;
+
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < n; j++)
+    {
+        x = new Random().NextDouble() * 10;
+        matrix[i, j] = Math.Round(x, 1);
+        Console.Write(matrix[i, j] + "\t");
+    }
+}
